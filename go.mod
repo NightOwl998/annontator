9e316aeeb27a0ffd10f4c29231c00eb0c4cec513
fadia <fadia@XLIM05040.sic.univ-poitiers.fr>
2023-02-08 16:43:21 +0100

    Pod not able to connect to localHost

--- go.mod
+++ go.mod
@@ -9,8 +9,18 @@ require (
 )
 
 require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.8.2 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.1 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
 )
 
 require (
@@ -38,17 +48,17 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.8.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
-	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/term v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -32,14 +34,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+	value := SendRequestAPI()
 	for _, node := range nodes.Items {
+		//we have one link so one bandwidth quality we can use in both cases, whereasœœ
+
 		annotations := node.GetAnnotations()
 		if annotations == nil {
 			annotations = make(map[string]string)
 		}
 
-		annotations["new_value"] = "it_worked"
+		annotations["Connexion_Quality"] = value
 
 		node.SetAnnotations(annotations)
 
@@ -48,7 +52,54 @@ func main() {
 		if err != nil {
 			fmt.Printf("Error updating node %s: %v\n", node.Name, err)
 		} else {
-			fmt.Printf("Successfully annotated node %s with key=value, yayy\n", node.Name)
+			fmt.Printf("Successfully annotated node %s with connexion value, yayy\n", node.Name)
 		}
 	}
+}
+func SendRequestAPI() string {
+	resp, err := http.Get("http://localhost:9090/wifi/isEnabled")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//Convert the body to type string
+	sb := string(body)
+	if sb == "true" {
+		resp, err := http.Get("http://localhost:9090/wifi/quality")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		//We Read the response body on the line below.
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		//Convert the body to type string
+		sb := string(body)
+		return sb
+	} else {
+		resp, err := http.Get("http://localhost:9090/lte/quality")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		//We Read the response body on the line below.
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		//Convert the body to type string
+		sb := string(body)
+		return sb
+
+	}
+
+	//log.Printf(sb)
+
 }
